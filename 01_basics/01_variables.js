9472b38cd50ebdd76b65f9b56074ef11ea0002eb
shashank-tyagi001 <mr.shashanktyagi007@icloud.com>
2024-07-17 00:31:25 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+
+const accountId = 1234;
+let accountEmail = "Shashank@gmail.com";
+var accountPasskey = "12345";
+
+
+
+
+accountEmail = "aryan@mail.com";
+accountPasskey = "88988";
+
+
+console.table([ accountEmail , accountPasskey  , accountId]);
